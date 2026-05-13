c09bd979873a3c4ffcd9336872ebbf240d6ca1a5
hector gustavo vallejo espinosa <taguivalle140965@gmail.com>
2026-05-13 07:18:43 +0200

    Proyecto Spring Boot generado - Estructura inicial

---
+++ src/main/java/com/kripxon/kripxonapp/KripxonAppApplication.java
@@ -0,0 +1,13 @@
+package com.kripxon.kripxonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KripxonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KripxonAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kripxon/kripxonapp/KripxonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kripxon.kripxonapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KripxonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
